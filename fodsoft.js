313d28053ea39b380d41de6a18942b06aea92424
FODSOFT™ <fodsoft@gmail.com>
2025-10-05 21:56:58 +0200

    Update fodsoft.js

--- fodsoft.js
+++ fodsoft.js
@@ -42,38 +42,32 @@ const traducciones = {
 		"game": "TOUS LES JEUX"
     },
 };
-
 const idiomasDisponibles = {
     "en": "en-US",
     "es": "es-ES",
     "fr": "fr-FR",
 };
-
 function detectarIdiomaNavegador() {
     const idiomaUsuario = navigator.language || navigator.userLanguage;
     const codigoIdioma = idiomaUsuario.split('-')[0];
     return idiomasDisponibles[codigoIdioma] ? codigoIdioma : 'en';
 }
-
 function obtenerIdioma() {
     const idiomaGuardado = localStorage.getItem('idioma');
     return idiomasDisponibles[idiomaGuardado] ? idiomaGuardado : detectarIdiomaNavegador();
 }
-
 function cambiarIdioma(nuevoIdioma) {
     if (idiomasDisponibles[nuevoIdioma]) {
         localStorage.setItem('idioma', nuevoIdioma);
         traducirPagina(nuevoIdioma);
     }
 }
-
 function traducirPagina(idioma) {
     document.querySelectorAll('[data-idioma]').forEach(elemento => {
         const clave = elemento.getAttribute('data-idioma');
         elemento.textContent = traducciones[idioma][clave] || clave;
     });
 }
-
 document.addEventListener('DOMContentLoaded', () => {
     const idioma = obtenerIdioma();
     traducirPagina(idioma);
@@ -84,10 +78,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const idiomaSeleccionado = evento.target.value;
         cambiarIdioma(idiomaSeleccionado);
     });
-
-
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-};
+document.addEventListener("DOMContentLoaded", function () {
+	const imgs = [
+		"https://resources.fodsoft.com/foto_nf.webp",
+		"https://resources.fodsoft.com/foto_neo_fodere.webp"
+	];
+	const imgAleatoria = imgs[Math.floor(Math.random() * imgs.length)];
+	const img = document.getElementById("fotoNF");
+	if (imagenElemento) { img.src = imgAleatoria; }
+});
+window.onbeforeunload = function () {window.scrollTo(0, 0);};
 });
 // FODSOFT(TM), NSTUDIOS GAMES(TM). Neo Fodere de Frutos. All rights reserved
