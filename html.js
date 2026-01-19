a52c10466cbf0a437434427e5ee11823b8cbc9a7
FODSOFT™ <fodsoft@gmail.com>
2026-01-19 22:19:22 +0100

    Update html.js

--- html.js
+++ html.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => 
 {
-	const header = `
+	const fodsoft-header = `
 		<div class="header-fodsoft">
 			<div class="menu-software">
 	            <img src="https://resources.fodsoft.com/images/webp/icono_software.webp"
@@ -69,6 +69,61 @@ document.addEventListener("DOMContentLoaded", () =>
 			</div>
         </div>
 	`;
-    document.getElementById("header-fodsoft").innerHTML = header;
+    document.getElementById("header-fodsoft").innerHTML = fodsoft-header;
+
+	const nsg-header = `
+		<div class="header-nsg">
+			<div class="menu-software">
+	            <img src="https://resources.fodsoft.com/images/webp/icono_software.webp"
+	                 class="icono-lista-software" draggable="false" oncontextmenu="return false">
+	            <div class="lista-software">
+					<a href="https://nstudios-games.fodsoft.com/fodfru">
+						<img src="https://resources.fodsoft.com/images/webp/icono_fodfru.webp"
+							class="icono-software" draggable="false" oncontextmenu="return false"> FodFru&trade;
+					</a>
+	            </div>
+			</div>
+
+	        <div class="logo-header-fodsoft">
+				<a href="https://nstudios-games.fodsoft.com/">
+		        	<img src="https://resources.fodsoft.com/images/webp/fodsoft_header.webp" class="logo-fodsoft-header"
+						draggable="false" oncontextmenu="return false;">
+				</a>
+            </div>
+
+            <nav class="links-header-fodsoft">
+                <a href="https://fodsoft.com" class="link-header-fodsoft">
+					<img src="https://resources.fodsoft.com/images/webp/icono_inicio.webp"
+						class="icono-header" draggable="false" oncontextmenu="return false"> Home</a>
+				<a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">
+					<img src="https://resources.fodsoft.com/images/webp/icono_juegos.webp"
+						class="icono-header" draggable="false" oncontextmenu="return false"> Games</a>
+				<a href="https://neofodere.fodsoft.com" class="link-header-fodsoft">
+					<img src="https://resources.fodsoft.com/images/webp/icono_acerca.webp"
+						class="icono-header" draggable="false" oncontextmenu="return false"> About</a>
+                <div id="selector-idiomas"></div>
+            </nav>
+
+			<div class="menu-nav">
+				<img src="https://resources.fodsoft.com/images/webp/icono_menu.webp"
+         			class="icono-lista-nav" draggable="false" oncontextmenu="return false">
+				<div class="lista-nav">
+					<a href="https://nstudios-games.fodsoft.com/">
+						<img src="https://resources.fodsoft.com/images/webp/icono_inicio.webp"
+							class="icono-software" draggable="false" oncontextmenu="return false"> Inicio
+					</a>
+					<a href="https:/fodsoft.com/">
+						<img src="https://resources.fodsoft.com/images/webp/icono_soft.webp"
+							class="icono-software" draggable="false" oncontextmenu="return false"> Games
+					</a>
+					<a href="https://neofodere.fodsoft.com/">
+						<img src="https://resources.fodsoft.com/images/webp/icono_acerca.webp"
+							class="icono-software" draggable="false" oncontextmenu="return false"> About
+					</a>
+				</div>
+			</div>
+        </div>
+	`;
+    document.getElementById("header-fodsoft").innerHTML = header-nsg;
 });
 // FODSOFT(TM). Neo Fodere de Frutos. All rights reserved.
