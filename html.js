21db7eaf86428a2e75426f65a66bd27bf00cabfc
FODSOFT™ <fodsoft@gmail.com>
2026-01-19 16:05:42 +0100

    Update html.js

--- html.js
+++ html.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => 
 {
 	const header = `
-		<div class="fs-header">
-            <div class="fs-header-left">
+		<div class="header">
+            <div class="logo-header">
                 <img src="https://resources.fodsoft.com/images/webp/fodsoft_header.webp" class="fs-header-logo">
             </div>
-            <nav class="fs-header-right">
-                <a href="https://fodsoft.com" class="fs-nav-item">Home</a>
-                <a href="https://nstudios-games.fodsoft.com" class="fs-nav-item">Games</a>
-                <a href="https://neofodere.fodsoft.com" class="fs-nav-item">About</a>
+            <nav class="links-header">
+                <a href="https://fodsoft.com" class="link-header">Home</a>
+                <a href="https://nstudios-games.fodsoft.com" class="link-headerm">Games</a>
+                <a href="https://neofodere.fodsoft.com" class="link-header">About</a>
                 <div id="language-selector"></div>
             </nav>
         </div>
