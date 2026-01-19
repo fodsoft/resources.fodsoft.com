352ec0f8c6dbd1d2ff7066f0121e92247a662cb3
FODSOFT™ <fodsoft@gmail.com>
2026-01-19 19:28:41 +0100

    Update html.js

--- html.js
+++ html.js
@@ -3,7 +3,8 @@ document.addEventListener("DOMContentLoaded", () =>
 	const header = `
 		<div class="header-fodsoft">
             <div class="logo-header-fodsoft">
-                <img src="https://resources.fodsoft.com/images/webp/fodsoft_header.webp" class="logo-fodsoft-header">
+                <img src="https://resources.fodsoft.com/images/webp/fodsoft_header.webp" class="logo-fodsoft-header"
+					draggable="false" oncontextmenu="return false;">
             </div>
             <nav class="links-header-fodsoft">
                 <a href="https://fodsoft.com" class="link-header-fodsoft">Home</a>
