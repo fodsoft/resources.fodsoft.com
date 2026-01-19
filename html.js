29e3a2630329d3b0fa6eae0bfdd7999aca95ff15
FODSOFT™ <fodsoft@gmail.com>
2026-01-19 22:49:59 +0100

    Update html.js

--- html.js
+++ html.js
@@ -39,13 +39,16 @@ document.addEventListener("DOMContentLoaded", () =>
             <nav class="links-header-fodsoft">
                 <a href="https://fodsoft.com" class="link-header-fodsoft">
 					<img src="https://resources.fodsoft.com/images/webp/icono_inicio.webp"
-						class="icono-header" draggable="false" oncontextmenu="return false"> Home</a>
+						class="icono-header" draggable="false" oncontextmenu="return false"> Home
+				</a>
 				<a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">
 					<img src="https://resources.fodsoft.com/images/webp/icono_juegos.webp"
-						class="icono-header" draggable="false" oncontextmenu="return false"> Games</a>
+						class="icono-header" draggable="false" oncontextmenu="return false"> Games
+				</a>
 				<a href="https://neofodere.fodsoft.com" class="link-header-fodsoft">
 					<img src="https://resources.fodsoft.com/images/webp/icono_acerca.webp"
-						class="icono-header" draggable="false" oncontextmenu="return false"> About</a>
+						class="icono-header" draggable="false" oncontextmenu="return false"> About
+				</a>
                 <div id="selector-idiomas"></div>
             </nav>
 
@@ -91,15 +94,18 @@ document.addEventListener("DOMContentLoaded", () =>
             </div>
 
             <nav class="links-header-fodsoft">
-                <a href="https://fodsoft.com" class="link-header-fodsoft">
+                <a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">
 					<img src="https://resources.fodsoft.com/images/webp/icono_inicio.webp"
-						class="icono-header" draggable="false" oncontextmenu="return false"> Home</a>
-				<a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">
+						class="icono-header" draggable="false" oncontextmenu="return false"> Home
+				</a>
+				<a href="https://fodsoft.com" class="link-header-fodsoft">
 					<img src="https://resources.fodsoft.com/images/webp/icono_soft.webp"
-						class="icono-header" draggable="false" oncontextmenu="return false"> Software</a>
+						class="icono-header" draggable="false" oncontextmenu="return false"> Software
+				</a>
 				<a href="https://neofodere.fodsoft.com" class="link-header-fodsoft">
 					<img src="https://resources.fodsoft.com/images/webp/icono_acerca.webp"
-						class="icono-header" draggable="false" oncontextmenu="return false"> About</a>
+						class="icono-header" draggable="false" oncontextmenu="return false"> About
+				</a>
                 <div id="selector-idiomas"></div>
             </nav>
 
