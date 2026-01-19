06ee35c8fe6548c88c637f92c67d0443ee434744
FODSOFT™ <fodsoft@gmail.com>
2026-01-19 19:35:31 +0100

    Update html.js

--- html.js
+++ html.js
@@ -7,9 +7,30 @@ document.addEventListener("DOMContentLoaded", () =>
 					draggable="false" oncontextmenu="return false;">
             </div>
             <nav class="links-header-fodsoft">
-                <a href="https://fodsoft.com" class="link-header-fodsoft">Home</a>
-                <a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">Games</a>
-                <a href="https://neofodere.fodsoft.com" class="link-header-fodsoft">About</a>
+                <a href="https://fodsoft.com" class="link-header-fodsoft">
+    <img src="https://resources.fodsoft.com/images/webp/icono_inicio.webp"
+         class="icono-header"
+         draggable="false"
+         oncontextmenu="return false">
+    Home
+</a>
+
+<a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">
+    <img src="https://resources.fodsoft.com/images/webp/icono_juegos.webp"
+         class="icono-header"
+         draggable="false"
+         oncontextmenu="return false">
+    Games
+</a>
+
+<a href="https://neofodere.fodsoft.com" class="link-header-fodsoft">
+    <img src="https://resources.fodsoft.com/images/webp/icono_acerca.webp"
+         class="icono-header"
+         draggable="false"
+         oncontextmenu="return false">
+    About
+</a>
+
                 <div id="selector-idiomas"></div>
             </nav>
         </div>
