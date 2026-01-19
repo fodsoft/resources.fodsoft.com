dc3523440add574b797ebc280488c84c3ce0dbfd
FODSOFT™ <fodsoft@gmail.com>
2026-01-19 20:23:32 +0100

    Update fodsoft.js

--- fodsoft.js
+++ fodsoft.js
@@ -13,10 +13,11 @@ document.addEventListener("DOMContentLoaded", () =>
 document.addEventListener("click", (e) => 
 {
     const menu = document.querySelector(".lista-software");
-    const icono = document.querySelector(".icono-software");
+    const icono = document.querySelector(".icono-lista");
     if (icono.contains(e.target))
         menu.style.display = menu.style.display === "flex" ? "none" : "flex";
     else
         menu.style.display = "none";
 });
 // FODSOFT(TM). Neo Fodere de Frutos. All rights reserved.
+
