f047fadd3b5cd57d05dac5ee950e78291f3f6479
FODSOFT™ <fodsoft@gmail.com>
2026-01-19 19:39:46 +0100

    Update html.js

--- html.js
+++ html.js
@@ -8,29 +8,14 @@ document.addEventListener("DOMContentLoaded", () =>
             </div>
             <nav class="links-header-fodsoft">
                 <a href="https://fodsoft.com" class="link-header-fodsoft">
-    <img src="https://resources.fodsoft.com/images/webp/icono_inicio.webp"
-         class="icono-header"
-         draggable="false"
-         oncontextmenu="return false">
-    Home
-</a>
-
-<a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">
-    <img src="https://resources.fodsoft.com/images/webp/icono_juegos.webp"
-         class="icono-header"
-         draggable="false"
-         oncontextmenu="return false">
-    Games
-</a>
-
-<a href="https://neofodere.fodsoft.com" class="link-header-fodsoft">
-    <img src="https://resources.fodsoft.com/images/webp/icono_acerca.webp"
-         class="icono-header"
-         draggable="false"
-         oncontextmenu="return false">
-    About
-</a>
-
+					<img src="https://resources.fodsoft.com/images/webp/icono_inicio.webp"
+						class="icono-header" draggable="false" oncontextmenu="return false"> Home</a>
+				<a href="https://nstudios-games.fodsoft.com" class="link-header-fodsoft">
+					<img src="https://resources.fodsoft.com/images/webp/icono_juegos.webp"
+						class="icono-header" draggable="false" oncontextmenu="return false"> Games</a>
+				<a href="https://neofodere.fodsoft.com" class="link-header-fodsoft">
+					<img src="https://resources.fodsoft.com/images/webp/icono_acerca.webp"
+						class="icono-header" draggable="false" oncontextmenu="return false"> About</a>
                 <div id="selector-idiomas"></div>
             </nav>
         </div>
