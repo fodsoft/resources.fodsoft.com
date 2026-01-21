a52e762952d0059c73dc769ab1db159cdc905cfd
FODSOFT™ <fodsoft@gmail.com>
2026-01-21 22:53:25 +0100

    Update html.js

--- html.js
+++ html.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () =>
 
 	        <div class="header-logo">
 				<a href="https://fodsoft.com/">
-		        	<img src="https://resources.fodsoft.com/images/webp/fodsoft_header.webp" class="logo-header quitar-suavizado-firefox"
+		        	<img src="https://resources.fodsoft.com/images/webp/fodsoft_header.webp" class="logo-header quitar-suavizado"
 						draggable="false" oncontextmenu="return false;">
 				</a>
             </div>
