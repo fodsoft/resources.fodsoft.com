1878901248b0125d0f8d1e391a481b87809e34ac
FODSOFT™ <fodsoft@gmail.com>
2026-01-22 15:46:44 +0100

    Update fodsoft.js

--- fodsoft.js
+++ fodsoft.js
@@ -16,12 +16,37 @@ document.addEventListener("DOMContentLoaded", () =>
 {
 	const selectorIdiomas = document.getElementById("selector-idiomas");
 	selectorIdiomas.innerHTML = `
-		<select id="fs-lang">
+		<select id="lista-idiomas">
 			<option value="en">EN</option>
 			<option value="es">ES</option>
 			<option value="fr">FR</option>
 		</select>
-	`;
+    `;
+	document.getElementById("lista-idiomas").addEventListener("change", (e) => 
+	{
+		cambiarIdioma(e.target.value);
+    });
 });
+async function cambiarIdioma(idioma)
+{
+    if (idioma === "en")
+		return;
+    const res = await fetch("https://resources.fodsoft.com/traducciones.json");
+    const traducciones = await res.json();
+    if (!traducciones[idioma])
+		return;
+    document.querySelectorAll("[data-idioma]").forEach(texto =>
+	{
+		const clave = texto.getAttribute("data-idioma");
+        if (traducciones[idioma][clave])
+		{
+            const padre = texto.parentElement;
+            padre.childNodes.forEach(node => 
+			{
+                if (node.nodeType === Node.TEXT_NODE)
+                    node.textContent = " " + traducciones[idioma][clave];
+			});
+		}
+    });
+}
 // FODSOFT(TM). Neo Fodere de Frutos. All rights reserved.
-
