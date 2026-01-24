af134077b249cf1ef1d331958a738ad83368ed44
FODSOFT™ <fodsoft@gmail.com>
2026-01-24 14:51:18 +0100

    Update html.js

--- html.js
+++ html.js
@@ -99,7 +99,7 @@ document.addEventListener("DOMContentLoaded", () =>
 	                </a>
 					<a href="https://linkedin.com/company/fodsoft" target="_blank">
 	                    <img src="https://resources.fodsoft.com/images/web/icono_linkedin.webp"
-	                         class="footer-enlace invertir-color pintar-rojo" draggable="false" oncontextmenu="return false">
+	                         class="footer-enlace invertir-color pintar-rojo redondear-bordes" draggable="false" oncontextmenu="return false">
 	                </a>
 					<a href="https://youtube.com/@fodsoft" target="_blank">
 	                    <img src="https://resources.fodsoft.com/images/web/icono_youtube.webp"
@@ -195,7 +195,7 @@ document.addEventListener("DOMContentLoaded", () =>
 	                </a>
 					<a href="https://www.linkedin.com/company/nstudios-games/" target="_blank">
 	                    <img src="https://resources.fodsoft.com/images/web/icono_linkedin.webp"
-	                         class="footer-enlace" draggable="false" oncontextmenu="return false">
+	                         class="footer-enlace redondear-bordes" draggable="false" oncontextmenu="return false">
 	                </a>
 					<a href="https://youtube.com/@nstudios-games" target="_blank">
 	                    <img src="https://resources.fodsoft.com/images/web/icono_youtube.webp"
