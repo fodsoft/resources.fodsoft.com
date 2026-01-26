8027b28d2d2677d8c5048a1fc58666667dd973ad
FODSOFT™ <fodsoft@gmail.com>
2026-01-26 23:40:18 +0100

    Update html.js

--- html.js
+++ html.js
@@ -222,4 +222,18 @@ document.addEventListener("DOMContentLoaded", () =>
 		footerNSG.innerHTML = nsg_footer;
 	}
 });
+
+document.addEventListener("click", (e) =>
+{
+    const menuSoft = document.querySelector(".lista-software");
+    if (e.target.closest(".icono-lista-software"))
+        menuSoft.style.display = menuSoft.style.display === "flex" ? "none" : "flex";
+    else
+        menuSoft.style.display = "none";
+    const menuNav = document.querySelector(".lista-nav");
+    if (e.target.closest(".icono-lista-nav"))
+        menuNav.style.display = menuNav.style.display === "flex" ? "none" : "flex";
+    else
+        menuNav.style.display = "none";
+});
 // FODSOFT(TM). Neo Fodere de Frutos. All rights reserved.
